2ab30f5bebef6c16abdb4a46cab17612372a5981
Lembahmanah <lembahku@gmail.com>
2025-10-01 12:17:19 +0700

    Percobaan 3

---
+++ jobsheet5/nestedUjianSkripsi13.java
@@ -0,0 +1,37 @@
+package jobsheet5;
+import java.util.Scanner;
+public class nestedUjianSkripsi13{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String pesan;
+        System.out.println("Apakah siswa sudah ebas kompen (Ya/Tidak)");
+        String bebasKompen = sc.nextLine().trim();
+
+        System.out.println("Masukkan jumlah log bimbingan Pembimbing 1: ");
+        int bimbinganP1 = sc.nextInt();
+
+        System.out.println("Masukkan jumlah log bimbingan Pembimbing 2: ");
+        int bimbinganP2 = sc.nextInt();
+
+        if (bebasKompen.equalsIgnoreCase("Ya")) {
+            if (bimbinganP1 >=8 && bimbinganP2 >=4) {
+                pesan = "Semua syarat terpenuhi. Mahasiswa boleh mendaftar ujian skripsi";
+                
+            }else if (bimbinganP1 <8 && bimbinganP2 <4){
+                pesan="Gagal! log bimbingan P1 kurang 8 kali dan P2 kurang dari 4 kali";
+                
+            }else if (bimbinganP1 < 8){
+                pesan="Gagal! log bimbingan P1 belum mencapai 8 kali";
+            }else{
+                pesan="Gagal! log bimbingan P2 belum mencapai 4 kali";
+            }
+            
+        }else{
+            pesan = "Gagal mahasiswa masih memiliki tanggungan kompen";
+        }
+        System.out.println(pesan);
+
+        
+    }
+}
